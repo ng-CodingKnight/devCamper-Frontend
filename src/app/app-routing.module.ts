8b4be9f77020f84abedef3b8a090511f454f90f8
ngCodingKnight <k.anujsingh10@gmail.com>
2022-01-22 18:19:21 +0530

    Added files and components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'landing', component: LandingPageComponent },
+  { path: 'login', component: LoginPageComponent },
+  { path: '**', component: LandingPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+}
